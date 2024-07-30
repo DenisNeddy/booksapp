6441ed5265944c218528e53958dc7029e8a45c15
DenisNeddy <65235810+DenisNeddy@users.noreply.github.com>
2024-07-30 14:05:51 +0300

    done 18.4 routing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,30 @@
-'use strict'
+import { MainView } from "./views/main/main";
 
-import './app.css';
+class App {
+    routes = [
+        {
+            path: "", view: MainView
+        }
+    ];
+    appState = {
+        favorites: []
+    }
 
+    constructor() {
+        window.addEventListener('hashchange', this.route.bind(this));
+        this.route();
+    }
+
+    route() {
+        if(this.currentView) {
+            this.currentView.destroy();
+        }
+        const view = this.routes.find(r => r.path == location.hash).view;
+        this.currentView = new view(this.appState);
+        console.log(this.currentView, 'currentView')
+        this.currentView.render();
+
+    }
+}
+
+new App();
