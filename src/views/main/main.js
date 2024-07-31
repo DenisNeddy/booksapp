3548d77c82a043c5c5ce748eb47a20dd61e197e5
DenisNeddy <65235810+DenisNeddy@users.noreply.github.com>
2024-07-31 14:49:35 +0300

    done component

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,5 +1,8 @@
 import { AbstractView } from "../../common/view";
 import onChange from "on-change";
+import { Header } from '../../components/header/header.js';
+import { Search } from "../../components/search/search.js";
+import { CardList } from "../../components/card-list/card-list.js";
 
 export class MainView extends AbstractView {
     state = {
@@ -13,24 +16,48 @@ export class MainView extends AbstractView {
         super();
         this.appState = appState;
         this.appState = onChange(this.appState, this.appStateHook.bind(this));
+        this.state = onChange(this.state, this.stateHook.bind(this));
         this.setTitle('Поиск книг');
     }
 
-    appStateHook(path) {
-        
+    appStateHook(path) {     
         if(path === 'favorites') {
             console.log(path);
         }
+    }
 
+    async stateHook(path) {      
+        if(path === 'searchQuery') {
+            this.state.loading = true;
+            const data = await this.loadList(this.state.searchQuery, this.state.offset)
+            this.state.loading = false;
+            this.state.list = data.docs;
+            console.log(this.state.searchQuery);
+            console.log(data); 
+        }
+
+        if(path === 'list' || path === 'loading') {
+            this.render();
+        }
+    }
+
+    async loadList(q,offset) {
+        const res = await fetch(`https://openlibrary.org/search.json?q=${q}&offset${offset}`);
+        return res.json(); 
     }
 
     render() {
         const main = document.createElement('div');
-        main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
-        console.log(this.app, '<---PIZDA')
-        console.dir(this)
+        main.append(new Search(this.state).render());
+        main.append(new CardList(this.appState, this.state).render());
         this.app.innerHTML = '';
         this.app.append(main);
-        this.appState.favorites.push('d');
+        this.renderHeader();
     } 
+
+    renderHeader() {
+        const header = new Header(this.appState).render();
+        this.app.prepend(header);
+
+    }
 }
